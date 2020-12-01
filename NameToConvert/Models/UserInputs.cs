331e1b80f96099ef880d5321d5585a92128809b1
Aykut Yucegun <32603097+aykutaskinyucegun@users.noreply.github.com>
2020-12-01 01:03:46 -0500

    upload
    repo

---
+++ NameToConvert/Models/UserInputs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converter.Models
+{
+    class UserInputs
+    {
+         public string FullName { get; set; }
+
+         public string binary { get; set; }
+
+        public char Character { get; set; }
+    }
+}
---
+++ NameToConvert/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Converter.Models;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Human!");
+
+            Console.WriteLine("Type your name to see how is it in my eyes!");
+
+            Create an instance of UserInput object
+           UserInputs userInputs = new UserInputs();
+            userInputs.FullName = Console.ReadLine();
+
+            BinaryConverter binaryConverter = new BinaryConverter();
+            string binaryValue = binaryConverter.ConvertTo(userInputs.FullName);
+            Console.WriteLine($"{userInputs.FullName} what I see as Binary: {binaryValue}");
+
+            Console.WriteLine("Now try the binary!");
+
+            userInputs.binary = Console.ReadLine();
+            Console.WriteLine($"{userInputs.binary} from Binary to ASCII: {binaryConverter.ConvertBinaryToString(binaryValue)}");
+
+            Console.WriteLine("Press é to see its hexadecimal!");
+
+            string input = userInputs.FullName;
+            string result = String.Concat(input.Select(x => ((int)x).ToString("x")));
+
+            Console.WriteLine($"{userInputs.FullName} from ASCI to hexadecimal : {result}");
+
+            ConsoleKeyInfo key = Console.ReadKey(); //this code takes user input
+            Console.Write(key.KeyChar);
+            userInputs.Character = key.KeyChar;
+            if (userInputs.Character == 'é')
+            {
+                HexadecimalConverter hexadecimalConverter = new HexadecimalConverter();
+                string hexavalue = hexadecimalConverter.ConvertTo(userInputs.FullName);
+                Console.WriteLine($"{userInputs.FullName} and this is the hexaform: {hexavalue}");
+            }
+            else
+
+                Console.WriteLine("opss so you don't want to learn it hexadeciaml!!");
+        }
+
+
+    }
+}
